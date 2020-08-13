a27f6e26c0c3196aa6ef41c60003f92dae2cdca0
Markychoco <67924592+Markychoco@users.noreply.github.com>
2020-08-13 16:15:57 +0300

    Add files via upload
    all the things you need if you are looking into creating a signup branch

---
+++ index.php
@@ -0,0 +1,51 @@
+<?php 
+	session_start(); 
+
+	if (!isset($_SESSION['username'])) {
+		$_SESSION['msg'] = "You must log in first";
+		header('location: login.php');
+	}
+
+	if (isset($_GET['logout'])) {
+		session_destroy();
+		unset($_SESSION['username']);
+		header("location: login.php");
+	}
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="header">
+		<h2>Markbook php,mysql and html Project</h2>
+	</div>
+	<div class="content">
+<h3>To be continued...</h3>
+		<!-- notification message -->
+		<?php if (isset($_SESSION['success'])) : ?>
+			<div class="error success" >
+				<h3>
+					<?php 
+						echo $_SESSION['success']; 
+						unset($_SESSION['success']);
+					?>
+				</h3>
+			</div>
+		<?php endif ?>
+
+		<!-- logged in user information -->
+		<?php  if (isset($_SESSION['username'])) : ?>
+			<p>Welcome <strong><?php echo $_SESSION['username']; ?></strong></p>
+			<p> <a href="index.php?logout='1'" style="color: red;">logout</a> </p>
+		<?php endif ?>
+	</div>
+<h1>MARKBOOK</h1>
+<p>A company that has taken its time to provide for you the best of the best.</p>
+<p>If you wish to check out more information click the button below.</p>
+<button class="button button2">More Info</button>	
+</body>
+</html>
